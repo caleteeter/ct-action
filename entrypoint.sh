e16428069561d6a6af801d8120d44ee6ab143316
Cale Teeter <cale.teeter@microsoft.com>
2022-10-24 23:50:39 -0400

    Updated perms

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-echo $SUPER_SECRET
-if [[ $1 == "testsecret" ]]; then
-  echo "found variable"
-else
-  echo "didn't find variable"
-fi
-
-#if [[ -z $1 ]]; then 
-#  echo "var found"
-#else
-#  echo "var not found";
-#fi
-
 echo "message=testmeout2" >> $GITHUB_OUTPUT
