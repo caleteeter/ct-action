07dcf070fbfb962f825d319d86c25ed31b9d6753
Cale Teeter <cale.teeter@microsoft.com>
2022-10-27 11:55:30 -0400

    Added voting

--- actions/vote/entrypoint.sh
+++ actions/vote/entrypoint.sh
@@ -5,5 +5,7 @@ cd /opt/ccf/bin
 proposal-id=$1
 url="https://identitylogapp.confidential-ledger.azure.com/gov/proposals/${proposal-id}/ballots"
 
+echo "${url}"
+
 content=$(./scurl.sh ${url} -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @accept.json)
 echo "status=$content" >> $GITHUB_OUTPUT
