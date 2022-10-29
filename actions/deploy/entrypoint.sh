191d51642638414609c291202ae7d7ae850ef404
Cale Teeter <cale.teeter@microsoft.com>
2022-10-29 01:30:12 -0400

    Updated ccfurl

--- actions/deploy/entrypoint.sh
+++ actions/deploy/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo ${ccfurl}
+
 cd /opt/ccf/bin
 content=$(./scurl.sh https://identitylogapp.confidential-ledger.azure.com/gov/proposals -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @set_js_app.json)
 proposal=$(echo "${content}" | jq '.proposal_id')
