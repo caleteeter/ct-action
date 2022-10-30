43975a1a65303ba0430d632a853f5d307360418a
Cale Teeter <cale.teeter@microsoft.com>
2022-10-30 10:58:41 -0400

    Update url for CCF from secrets

--- actions/deploy/entrypoint.sh
+++ actions/deploy/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+url=$(eval echo $ccfurl)
+
 cd /opt/ccf/bin
-content=$(./scurl.sh https://identitylogapp.confidential-ledger.azure.com/gov/proposals -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @set_js_app.json)
+content=$(./scurl.sh ${url}gov/proposals -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @set_js_app.json)
 proposal=$(echo "${content}" | jq '.proposal_id')
 echo "proposal=$proposal" >> $GITHUB_OUTPUT
--- actions/vote/entrypoint.sh
+++ actions/vote/entrypoint.sh
@@ -3,7 +3,8 @@
 cd /opt/ccf/bin
 
 pp=$(eval echo $proposal)
-url=https://identitylogapp.confidential-ledger.azure.com/gov/proposals/${pp}/ballots
+uri=$(eval echo $ccfurl)
+url=${uri}gov/proposals/${pp}/ballots
 
 content=$(./scurl.sh ${url} -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @accept.json)
 status=$(echo "$content" | jq '.state')
