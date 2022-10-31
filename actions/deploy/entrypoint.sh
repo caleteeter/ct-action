2e22cec0c535824ce9a83095f38ab719dd1cd316
Cale Teeter <cale.teeter@microsoft.com>
2022-10-31 12:27:43 -0400

    Update ref to input application bundle

--- actions/deploy/entrypoint.sh
+++ actions/deploy/entrypoint.sh
@@ -2,10 +2,6 @@
 
 url=$(eval echo $ccfurl)
 
-echo "Application: $1"
-echo $application
-echo $input_application
-
 cd /opt/ccf/bin
 content=$(./scurl.sh ${url}gov/proposals -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @set_js_app.json)
 proposal=$(echo "${content}" | jq '.proposal_id')
