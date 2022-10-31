53e2e032332084804c40e90fb2fd7e48eafc9b8f
Cale Teeter <cale.teeter@microsoft.com>
2022-10-31 14:30:36 -0400

    Update ref to input application bundle

--- actions/vote/entrypoint.sh
+++ actions/vote/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd /opt/ccf/bin
+pp=$(eval $2)
 
 content=$(./scurl.sh $1gov/proposals/$2/ballots -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @accept.json)
-echo "$1/gov/proposals/$2/ballots"
+echo "$1gov/proposals/$pp/ballots"
 status=$(echo "$content" | jq '.state')
 echo "status=$status" >> $GITHUB_OUTPUT
