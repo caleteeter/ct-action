6098e54b6c1c8201768122c926318b0eaa782cd0
Cale Teeter <cale.teeter@microsoft.com>
2022-10-31 15:02:07 -0400

    Update ref to input application bundle

--- actions/deploy/entrypoint.sh
+++ actions/deploy/entrypoint.sh
@@ -2,9 +2,7 @@
 
 cp $1 /opt/ccf/bin
 
-echo "${CCF_URL}gov/proposals"
-
 cd /opt/ccf/bin
-content=$(./scurl.sh $CCF_URLgov/proposals -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @set_js_app.json)
+content=$(./scurl.sh ${CCF_URL}gov/proposals -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @set_js_app.json)
 proposal=$(echo "${content}" | jq '.proposal_id')
 echo "proposal=$proposal" >> $GITHUB_OUTPUT
--- actions/vote/entrypoint.sh
+++ actions/vote/entrypoint.sh
@@ -3,6 +3,6 @@
 cd /opt/ccf/bin
 proposal_id=$(eval echo $1)
 
-content=$(./scurl.sh $CCF_URLgov/proposals/$proposal_id/ballots -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @accept.json)
+content=$(./scurl.sh ${CCF_URL}gov/proposals/$proposal_id/ballots -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @accept.json)
 status=$(echo "$content" | jq '.state')
 echo "status=$status" >> $GITHUB_OUTPUT
