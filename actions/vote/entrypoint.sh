4b56ae0216d59553c33791b3745872eef098068a
Cale Teeter <cale.teeter@microsoft.com>
2022-11-01 01:07:45 -0400

    Testing

--- actions/vote/entrypoint.sh
+++ actions/vote/entrypoint.sh
@@ -3,6 +3,9 @@
 cd /opt/ccf/bin
 proposal_id=$(eval echo $1)
 
+cat ${CERTD} > /opt/ccf/bin/cert
+cat ${KEYD} > /opt/ccf/bin/key
+
 content=$(./scurl.sh ${CCF_URL}gov/proposals/$proposal_id/ballots -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @accept.json)
 status=$(echo "$content" | jq '.state')
 echo "status=$status" >> $GITHUB_OUTPUT
